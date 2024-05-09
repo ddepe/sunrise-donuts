a1e696e347ab114ee85cb3d85919b47f53a747ec
Danny Depe <ddepe@g.ucla.edu>
2024-05-08 18:44:56 -0700

    Move update to scripts

--- scripts/update_sales_data.py
+++ scripts/update_sales_data.py
@@ -9,8 +9,8 @@ Dependencies:
 - `sales_data_util`: A utility module containing functions for managing and updating sales data.
 """
 
-import sales_data_util
+import squareup.sales_data_util
 
-last_date = sales_data_util.get_last_update_date('data/aggregated_sales.csv') 
+last_date = squareup.sales_data_util.get_last_update_date('data/aggregated_sales.csv') 
 
-sales_data_util.update_history(last_date)
+squareup.sales_data_util.update_history(last_date)
